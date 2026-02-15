11c10adb91848c7cd1a5239b4553d117917a8c01
Nurdaulet <robloxgeimer21@gmail.com>
2026-02-15 23:40:49 +0500

    Add initial implementations for lab1, lab2, lab3, and lab4 with various functionalities

---
+++ lab1a/lab1.java
@@ -0,0 +1,6 @@
+import java.time.LocalTime;
+
+public class labb1 {
+    public static void main(String[] args) {
+        System.out.println("Қазіргі уақыт: " + LocalTime.now());
+    }
---
+++ lab2/lab2.java
@@ -0,0 +1,25 @@
+class lab2 {
+
+    void show1() {
+        System.out.println("Бұл fclass1 класы");
+    }
+
+    static void main(String[] args) {
+        fclass2 obj = new lab2();
+        obj.show1();
+        obj.show3();
+        obj.show2();
+    }
+}
+
+class fclass3 extends fclass1 {
+    void show3() {
+        System.out.println("Бұл fclass3 класы");
+    }
+}
+
+class fclass2 extends fclass3 {
+    void show2() {
+        System.out.println("Бұл fclass2 класы");
+    }
+}
